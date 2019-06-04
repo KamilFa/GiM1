cef022a8147cea5d75b1d908a42d049ef29d3151
Kamil <kamfab44@gmail.com>
2019-06-04 19:38:57 +0200

    Labolatoria
    dd

---
+++ Lab2/Assets/Scripts/SimplePlatformController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimplePlatformController : MonoBehaviour
+{
+    [HideInInspector] public bool facingright = true;
+    [HideInInspector] public bool jump = false;
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Transform groundcheck;
+
+    private bool grounded = false;
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundcheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jump = true;
+        }
+               
+    }
+
+    void Flip()
+    {
+        facingright = !facingright;
+        Vector3 tempScale = transform.localScale;
+        tempScale.x *= -1;
+        transform.localScale = tempScale;
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", Mathf.Abs (h));
+
+        if (h * rb2d.velocity.x < maxSpeed)
+        {
+            rb2d.AddForce(Vector2.right * h * moveForce);
+        }
+        if (Mathf.Abs (rb2d.velocity.x)>maxSpeed)
+        {
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+        }
+        if (h>0 && !facingright)
+            {
+            Flip();
+            }
+        else if (h<0 && facingright)
+        {
+            Flip();
+        }
+        if (jump)
+        {
+            anim.SetTrigger("Jump");
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        
+        }
+
+    }
+}
---
+++ Lab4v1/Assets/Standard Assets/Cameras/Scripts/TriggerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour
+{
+    public GameObject LightG;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+        {
+            LightG.GetComponent<Light>().color = Color.red;
+            AudioSource sound = other.GetComponent<AudioSource>();
+            sound.Play();
+        }
+    }
+}
